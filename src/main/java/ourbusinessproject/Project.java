3992d0c5d62c385191f349e8f8c6f6930480a927
Andrioaie Adrian <adrian.andrioaie2005@gmail.com>
2026-01-27 10:22:52 +0100

    fix #1.1 classe Project

---
+++ src/main/java/ourbusinessproject/Project.java
@@ -0,0 +1,21 @@
+package ourbusinessproject;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public class Project {
+    @NotEmpty
+    @NotNull
+    private String title;
+    private String description;
+
+    public Project () {}
+
+    public void setTitle(String title) {
+            this.title = title;
+    }
+
+    public void setDescription(String description) {
+            this.description = description;
+    }
+}
